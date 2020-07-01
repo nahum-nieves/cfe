992f732bb0b5e4de5c62b5bc420c55b6eaa841b3
nahum-nieves <60203071+nahum-nieves@users.noreply.github.com>
2020-07-01 18:37:38 -0500

    VIsual Estudio
    Visual Studio code

---
+++ CFE/WCFService1/App_Code/IMedicionService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MedicionService
+{
+
+    [ServiceContract]
+    public interface IMedicionesService
+    {
+        [OperationContract]
+        string AgregaMediciones(Medicion mediciones);
+
+    }
+
+   // [DataContract]
+  //  public class Medidor
+  //  {
+  //      [DataMember]
+  //      public string Clave { get; set; }
+  //      [DataMember]
+  //      public Medicion[] Mediciones { get; set; }
+
+
+  //  }
+
+    [DataContract]
+    public class Medicion
+    {
+        [DataMember]
+        public string Clave
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public Valor[] Mediciones
+        {
+            get;
+            set;
+        }
+
+    }
+
+    [DataContract]
+    public class Valor
+    {
+
+        [DataMember]
+        public DateTimeOffset Fecha
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public double Valor
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ CFE/WCFService1/App_Code/MedicionService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+
+namespace MedicionService
+{
+
+    public class MedicionService : IMedicionesService
+    {
+        public string InsertaMediciones(Medicion mediciones){
+           
+            
+            var json = JsonConvert.SerializeObject(mediciones);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+           
+
+
+
+
+            HttpWebRequest request = WebRequest.Create("https://arena-dev.nxtlab.mx/api/cfe/mediciones") as HttpWebRequest;
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+            string respuesta = "Respuesta del WS";
+            HttpWebResponse response = request.GetResponse() as HttpWebResponse;
+            return (respuesta);
+        }
+
+        public string AgregaMediciones(Medicion mediciones){
+            //string resultado = "OK";
+            string resultado = InsertaMediciones(mediciones);
+            return resultado;
+
+        }
+    }   
+}
